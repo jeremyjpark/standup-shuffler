7f6d430fc606546430522d63e4cc22ef75ee63a7
Jeremy Park <jeremyjpark@gmail.com>
2019-06-13 23:46:03 -0700

    update name

--- shuffle.rb
+++ shuffle.rb
@@ -5,5 +5,6 @@ require 'yaml'
 data = YAML.load_file(File.expand_path("./data.yml"))
 
 post('/') do
-  HTTParty.post(data['url'], body: { text: data['team_members'].shuffle.join(', '), channel: "\##{params['channel_name']}", icon_emoji: ':twisted_rightwards_arrows:', username: 'slack-shuffle' }.to_json )
+  puts data['team_members'].shuffle.join(', ')
+  HTTParty.post(data['url'], body: { text: data['team_members'].shuffle.join(', '), channel: "\##{params['channel_name']}", icon_emoji: ':twisted_rightwards_arrows:', username: 'STANdup' }.to_json )
 end
